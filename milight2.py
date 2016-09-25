76e4a508de47d96a7475a72042df508f12291c25
Patrick Hanckmann <patrick.hanckmann@tno.nl>
2016-09-25 20:18:14 +0200

    added an option parser and example executable

---
+++ milight2.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+""" MiLight Commandline Interface """
+
+import argparse
+import mci
+import parser
+
+
+def main():
+    """ Main. """
+    # Parse commandline arguments
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-s', '--scan', action='store_true', dest='scan',
+                        help='Search wifi bridges.', default=False, required=False)
+
+    argparser.add_argument('-i', '--ip_address', action='store', nargs=1, dest='address',
+                        help='IP address.', default='192.168.0.230', required=False, type=str)
+    argparser.add_argument('-p', '--port', action='store', nargs=1, dest='port',
+                        help='Port number.', default=8899, required=False, type=int)
+
+    argparser.add_argument('-c', '--rgbw', action='store', nargs=1, dest='rgbw',
+                        help='Set RGBW target group, default: None (1, 2, 3, 4, ALL)', default=None, required=False)
+    argparser.add_argument('-w', '--white', action='store', nargs=1, dest='white',
+                        help='Set White target group, default: None (1, 2, 3, 4, ALL)', default=None, required=False)
+
+    argparser.add_argument('-a', '--action', action='store', nargs='*', dest='action',
+                        help='The desired action, default: None (for white bulbs/strips: ON, OFF, INC_BRIGHTNESS, DEC_BRIGHTNESS, INC_WARMTH, DEC_WARMTH, BRIGHT_MODE, NIGHT_MODE | for rgbw bulbs/strips: ON, OFF, DISCO_MODE, INC_DISCO_SPEED, DEC_DISCO_SPEED, as arguments )', default=None, required=False)
+
+    argparser.add_argument('--colors', action='store_true', dest='show_colors',
+                        help='Show supported colors', default=False, required=False)
+    argparser.add_argument('--disco_modes', action='store_true', dest='show_disco_modes',
+                        help='Show supported disco modes', default=False, required=False)
+    argparser.add_argument('-v', '--verbose', action='store_true', dest='verbose',
+                        help='Show some (more) details of what is happening', default=False, required=False)
+
+    args = argparser.parse_args()
+
+    # Set the bridge ip address and port
+    bridge = args.address[0]
+    # port = args.port
+
+    # Scan for wifi bridges (to find the ip address)
+    if args.scan:
+        print('Discovering bridges')
+        dg = mci.DiscoverBridge(port=48899).discover()
+        for (addr, mac) in dg:
+            print(' - ip address :' + addr + '\tmac: ' + mac)
+
+    if args.show_colors:
+        print('Supported colors:')
+        for cc in mci.ColorGroup.COLOR_CODES.keys():
+            print(' - ' + str(cc))
+
+    if args.show_disco_modes:
+        print('Supported disco modes:')
+        for dc in mci.ColorGroup.DISCO_CODES.keys():
+            print(' - ' + str(dc))
+
+    # Set the bulb-type and group
+    bulb = None
+    group = None
+    if args.rgbw is not None:
+        bulb = 'RGBW'
+        group = args.rgbw[0]
+    elif args.white is not None:
+        bulb = 'WHITE'
+        group = args.white[0]
+
+    # Set action
+    action = None
+    value = None
+    if len(args.action) > 0:
+        action = args.action[0]
+    if len(args.action) > 1:
+        args.action.pop(0)
+        value = " ".join(args.action)
+
+    if args.verbose:
+        print('Details:')
+        print(' bridge: ' + str(bridge))
+        print(' bulb: ' + str(bulb))
+        print(' group: ' + str(group))
+        print(' action: ' + str(action))
+        print(' value: ' + str(value))
+
+    # Parse, validate, and execute the requested command
+    parser.execute_command(bridge, bulb, group, action, value)
+
+
+if __name__ == '__main__':
+    main()
---
+++ parser.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+""" MiLight Control Interface Parser
+
+A powerful parser to use the MCI Python API to control your
+MiLight LED bulbs and strips (White and RGBW).
+"""
+
+import mci
+
+
+class MCIParserException(Exception):
+    pass
+
+
+def parse_bridge(bridge):
+    """ Parser and validator for the bridge information """
+    return bridge
+    # raise MCIParserException('Provided bridge not available')
+
+
+def parse_bulb(bulb):
+    """ Parser and validator for the bulb information """
+    bulb = bulb.upper()
+    if bulb in ['COLOR', 'RGBW']:
+        return 'RGBW'
+    elif bulb in ['WHITE']:
+        return 'WHITE'
+    else:
+        raise MCIParserException('Provided bulb-type not valid')
+
+
+def parse_group(group):
+    """ Parser and validator for the group information """
+    group = str(group).upper()
+    if group in ['ALL', '0', '*']:
+        return 'ALL'
+    elif group in ['1', '2', '3', '4']:
+        return group
+    else:
+        raise MCIParserException('Provided group description not valid')
+
+
+def parse_action(action, bulb):
+    """ Parser and validator for the action information (depends on the bulb-type) """
+    action = action.upper()
+    if action in ['ON', 'OFF']:
+        return action
+    if parse_bulb(bulb) in ['RGBW']:
+        # Check for RBWG actions
+        if action in ['WHITE', 'BRIGHTNESS', 'DISCO', 'INCREASE_DISCO_SPEED', 'DECREASE_DISCO_SPEED', 'COLOR']:
+            return action
+    if parse_bulb(bulb) in ['WHITE']:
+        # Check for WHITE actions
+        if action in ['INCREASE_BRIGHTNESS', 'DECREASE_BRIGHTNESS', 'INCREASE_WARMTH', 'DECREASE_WARMTH', 'BRIGHTMODE', 'NIGHTMODE']:
+            return action
+    MCIParserException('Provided action not supported')
+
+
+def parse_value(value, action):
+    """ Parser and validator for the value information (depends on the action) """
+    # For RGBW bulbs
+    if not value:
+        return value
+    if action == 'BRIGHTNESS':
+        value = int(value)
+        if value >= 0 and value <= 25:
+            return value
+        else:
+            raise MCIParserException('Provided value not valid')
+    if action == 'DISCO':
+        value = value.upper()
+        if value in mci.ColorGroup.DISCO_CODES.keys():
+            return value
+        else:
+            raise MCIParserException('Provided value not valid')
+    if action in ['INCREASE_DISCO_SPEED', 'DECREASE_DISCO_SPEED']:
+        value = int(value)
+        if value >= 1 and value <= 30:
+            return value
+        else:
+            raise MCIParserException('Provided value not valid')
+    if action == 'COLOR':
+        value = value.upper()
+        if value in mci.ColorGroup.COLOR_CODES.keys():
+            return value
+        else:
+            raise MCIParserException('Provided value not valid')
+    # For WHITE bulbs
+    if action in ['INCREASE_BRIGHTNESS', 'DECREASE_BRIGHTNESS', 'INCREASE_WARMTH', 'DECREASE_WARMTH']:
+        value = int(value)
+        if value >= 1 and value <= 30:
+            return value
+        else:
+            raise MCIParserException('Provided value not valid')
+    return None
+
+
+def validate_command(bridge, bulb, group, action, value):
+    """ Parses, and validates the requested command """
+    bridge = parse_bridge(bridge)
+    bulb = parse_bulb(bulb)
+    group = parse_group(group)
+    action = parse_action(action, bulb)
+    value = parse_value(value, action)
+    return (True, bridge, bulb, group, action, value)
+
+
+def execute_command(bridge, bulb, group, action, value):
+    """ Parses, validates, and executes the requested command """
+    (valid, bridge, bulb, group, action, value) = validate_command(bridge, bulb, group, action, value)
+    if not valid:
+        MCIParserException('Invalid command provided')
+
+    if bulb is 'RGBW':
+        lc = mci.ColorGroup(ip_address=bridge, group_number=group)
+        if action == 'ON':
+            lc.on()
+        elif action == 'OFF':
+            lc.off()
+        elif action == 'WHITE':
+            lc.white()
+        elif action == 'BRIGHTNESS':
+            lc.brightness(value)
+        elif action == 'DISCO':
+            lc.disco()
+        elif action == 'INCREASE_DISCO_SPEED':
+            lc.increase_disco_speed()
+        elif action == 'DECREASE_DISCO_SPEED':
+            lc.decrease_disco_speed()
+        elif action == 'COLOR':
+            lc.color(value)
+    elif bulb is 'WHITE':
+        lc = mci.WhiteGroup(ip_address=bridge, group_number=group)
+        if action == 'ON':
+            lc.on()
+        if action == 'OFF':
+            lc.off()
+        if action == 'INCREASE_BRIGHTNESS':
+            lc.increase_brightness()
+        if action == 'DECREASE_BRIGHTNESS':
+            lc.decrease_brightness()
+        if action == 'INCREASE_WARMTH':
+            lc.increase_warmth()
+        if action == 'DECREASE_WARMTH':
+            lc.decrease_warmth()
+        if action == 'BRIGHTMODE':
+            lc.brightmode()
+        if action == 'NIGHTMODE':
+            lc.nightmode()
