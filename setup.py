88234f0b3436ca0828fdde2e373ac89c6b2abf19
Patrick Hanckmann <patrick.hanckmann@tno.nl>
2016-10-24 21:07:09 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 from setuptools import setup, find_packages
-import sys, os
+import sys
+import os
 
-version = '1.0.2'
+version = '1.1.0'
 
 setup(name='mci',
       version=version,
       description='MiLight-Control-Interface',
       long_description='''The MiLight Control Interface is a powerful Python API to control MiLight LED bulbs and strips (White and RGBW).''',
       classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Other Audience',
-        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Home Automation',
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Other Audience',
+          'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Home Automation',
       ],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='milight interface',
+      keywords='milight led light interface',
       author='Patrick Hanckmann',
       author_email='hanckmann@gmail.com',
       url='https://hanckmann.com',
